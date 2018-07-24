897c9261713f88da28012e597d4cf5a40d2f0605
SteveNay <naylinaung.softwarearchitect@gmail.com>
2018-07-24 23:26:34 +0630

    Update Database config

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$dbUrl = parse_url(env("CLEARDB_DATABASE_URL"));
 
 return [
 
@@ -41,11 +42,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+
+            'host' => isset($dbUrl["host"]) ? $dbUrl["host"] : env("DB_HOST"),
+            'database' => isset($dbUrl["path"]) ? substr($dbUrl["path"], 1) : env("DB_DATABASE"),
+            'username' => isset($dbUrl["user"]) ? $dbUrl["user"] : env("DB_USERNAME"),
+            'password' => isset($dbUrl["pass"]) ? $dbUrl["pass"] : env("DB_PASSWORD"),
+
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
